d280d15a348c34031fc15f220e5cf0ed9f7a28b3
Zhiyuan Ma <631253076@qq.com>
2021-03-18 17:21:22 +0800

    spring boot learning first lesson

---
+++ hello/src/test/java/com/squirrel/hello/HelloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.squirrel.hello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
