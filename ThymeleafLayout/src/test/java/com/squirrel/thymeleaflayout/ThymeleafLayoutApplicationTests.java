98557a93ce5df379605b651d43112bd07d92d2d9
Zhiyuan Ma <631253076@qq.com>
2021-04-23 04:57:16 +0800

    spring boot learning 2-5 Thymeleaf layout

---
+++ ThymeleafLayout/src/test/java/com/squirrel/thymeleaflayout/ThymeleafLayoutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.squirrel.thymeleaflayout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafLayoutApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
